a84d2da13cbd1e9ae83783249e959b4510d23039
Muhammad Ihsan <mihsan191@gmail.com>
2017-05-30 15:03:43 +1000

    Allow send Delete request to a book

--- src/Library.API/Controllers/BooksController.cs
+++ src/Library.API/Controllers/BooksController.cs
@@ -57,5 +57,19 @@ namespace Library.API.Controllers
             var bookToReturn = Mapper.Map<BookDto>(bookEntity);
             return CreatedAtRoute("GetBookForAuthor", new { authorId = authorId, id = bookToReturn.Id }, bookToReturn);
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBookForAuthor(Guid authorId, Guid id)
+        {
+            if (!_libraryRepository.AuthorExists(authorId)) return NotFound();
+
+            var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId, id);
+            if (bookForAuthorFromRepo == null) return NotFound();
+
+            _libraryRepository.DeleteBook(bookForAuthorFromRepo);
+            if (!_libraryRepository.Save()) throw new Exception($"Deleting book {id} for author {authorId} failed on save.");
+
+            return NoContent();
+        }
     }
 }
