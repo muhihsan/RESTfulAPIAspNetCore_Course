2c24a7efe2a22fd48d8050dd3fbf1f120d7175be
Muhammad Ihsan <mihsan191@gmail.com>
2017-06-05 11:10:24 +1000

    Add logging to debug window

--- src/Library.API/Startup.cs
+++ src/Library.API/Startup.cs
@@ -13,6 +13,7 @@ using Library.API.Entities;
 using Library.API.Helpers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Diagnostics;
 
 namespace Library.API
 {
@@ -56,6 +57,7 @@ namespace Library.API
             ILoggerFactory loggerFactory, LibraryContext libraryContext)
         {
             loggerFactory.AddConsole();
+            loggerFactory.AddDebug(LogLevel.Information);
 
             if (env.IsDevelopment())
             {
@@ -63,8 +65,19 @@ namespace Library.API
             }
             else
             {
-                app.UseExceptionHandler(appBuilder => {
-                    appBuilder.Run(async context => {
+                app.UseExceptionHandler(appBuilder => 
+                {
+                    appBuilder.Run(async context => 
+                    {
+                        var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+                        if (exceptionHandlerFeature != null)
+                        {
+                            var logger = loggerFactory.CreateLogger("Global exception logger");
+                            logger.LogError(500,
+                                exceptionHandlerFeature.Error,
+                                exceptionHandlerFeature.Error.Message);
+                        }
+
                         context.Response.StatusCode = 500;
                         await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
                     });
